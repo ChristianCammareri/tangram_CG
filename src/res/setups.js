b9e2c13d0c6dc4d0a60cf3195a62d7bcaf168814
ChristianCammareri <38110773+ChristianCammareri@users.noreply.github.com>
2020-07-12 12:32:12 +0200

    Update coordinates

--- src/res/setups.js
+++ src/res/setups.js
@@ -22,6 +22,17 @@ const swanSetup = [
 
 
 const initialSetup = [
+    [-Math.sqrt(2)/3, 0.0, 225.0],
+    [0.0, Math.sqrt(2)/3, 315.0],
+    [Math.sqrt(2)/6, 0.0, 45.0],
+    [-Math.sqrt(2)/4, -5*Math.sqrt(2)/12, 135.0],
+    [Math.sqrt(2)/3, -Math.sqrt(2)/3, 270.0],
+    [0.0, -Math.sqrt(2)/4, 45.0],
+    [3*Math.sqrt(2)/8, Math.sqrt(2)/8, 90.0]
+];
+
+
+const initialSetupOld= [ //da cancallare
     [0, 0, 225.0],
     [0, 0, 315.0],
     [0, 0, 45.0],
